8f110b9e600a2e1b1c1d239025d10eed63c87e11
Giap Le <giap.le@s3corp.com.vn>
2019-07-11 16:23:08 +0700

    add menu side model

---
+++ src/models/SideMenuModel.js
@@ -0,0 +1,82 @@
+export const SIDE_MENU = [
+  {
+    name: 'Thông tin giáo xứ',
+    subMenu: [
+      {
+        name: 'Giáo xứ',
+        icon: '',
+      },
+      {
+        name: 'Giáo họ',
+        icon: '',
+      },
+    ],
+    icon: '',
+  },
+  {
+    name: 'Giáo dân và gia đình',
+    subMenu: [
+      {
+        name: 'Danh sách giáo dân',
+        icon: '',
+      },
+      {
+        name: 'Danh sách gia đình',
+        icon: '',
+      },
+    ],
+    icon: '',
+  },
+  {
+    name: 'Hồ sơ lưu trữ',
+    subMenu: [
+      {
+        name: 'Hồ sơ lưu giữ giáo dân',
+        icon: '',
+      },
+      {
+        name: 'Hồ sơ lưu giữ gia đình',
+        icon: '',
+      },
+    ],
+    icon: '',
+  },
+  {
+    name: 'Bí tích',
+    subMenu: [
+      {
+        name: 'Danh sách sổ bí tích',
+        icon: '',
+      },
+      {
+        name: 'danh sách rao hôn phối',
+        icon: '',
+      },
+    ],
+    icon: '',
+  },
+  {
+    name: 'Thống kê',
+    subMenu: [
+      {
+        name: 'Thống kê chung',
+        icon: '',
+      },
+      {
+        name: 'Biểu đồ',
+        icon: '',
+      },
+    ],
+    icon: '',
+  },
+  {
+    name: 'Quản lý giáo lý',
+    subMenu: [
+      {
+        name: 'Danh sách khối lớp',
+        icon: '',
+      },
+    ],
+    icon: '',
+  },
+];
